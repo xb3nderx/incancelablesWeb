af37578586e9b4cb9dc7b77375f7318657d02d6d
Facundo Araujo <facundoaraujo@gmail.com>
2026-07-21 21:34:32 -0300

    se configuran los back prod y dev

--- scripts/api/apiConfig.js
+++ scripts/api/apiConfig.js
@@ -4,8 +4,19 @@
 
 const API = {
 
-    URL: "https://script.google.com/macros/s/AKfycbwCdemjNBvYsy6MsqtEpfqjfctOGdxCK_S2ukxLQmCfM6TKXAwvPODOrruMngm9r5C8/exec",
+    URL_DEV:
+        "https://script.google.com/macros/s/AKfycbzMSx5lM48xVjKBwxpw-51y3unfKOXS8eb7EuqOrviNFSLnX1xQ36Jwvkpz1bbNwu0o/exec",
+
+    URL_PROD:
+        "https://script.google.com/macros/s/AKfycbwCdemjNBvYsy6MsqtEpfqjfctOGdxCK_S2ukxLQmCfM6TKXAwvPODOrruMngm9r5C8/exec",
+
+    ENVIRONMENT: "PROD",
 
     TIMEOUT: 10000
 
 };
+
+API.URL =
+    API.ENVIRONMENT === "PROD"
+        ? API.URL_PROD
+        : API.URL_DEV;
